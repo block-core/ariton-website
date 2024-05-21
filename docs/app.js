b8a65464759ba6be34d864f51de9ad56afa8436e
Milad Raeisi <miladsoft@yahoo.com>
2024-05-22 00:44:24 +0400

    Update website

---
+++ docs/app.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbzMwKjd1Bdrk8PAVXhuJi_EnuZnAipQG7Mgy9XLGfuj3i9N7O3-7sugqET2CkK89V_Q/exec';
+    const form = document.getElementById('investment-form');
+    const submitting = document.querySelector('.js-submitting');
+    const successMessage = document.querySelector('.js-success-message');
+    const errorMessage = document.querySelector('.js-error-message');
+
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+        showLoadingIndicator();
+        fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+            .then(response => showSuccessMessage(response))
+            .catch(error => showErrorMessage(error));
+    });
+
+    function showLoadingIndicator() {
+        submitting.classList.remove('is-hidden');
+    }
+
+    function showSuccessMessage(response) {
+        console.log('Success!', response);
+        setTimeout(() => {
+            successMessage.classList.remove('is-hidden');
+            submitting.classList.add('is-hidden');
+        }, 500);
+    }
+
+    function showErrorMessage(error) {
+        console.error('Error!', error.message);
+        setTimeout(() => {
+            errorMessage.classList.remove('is-hidden');
+            submitting.classList.add('is-hidden');
+        }, 500);
+    }
+});
