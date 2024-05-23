bfd2a59a77acef7612b4c03c4dbe432eeb5e1fc9
Milad Raeisi <miladsoft@yahoo.com>
2024-05-23 18:55:36 +0400

    fixed all the styles and added our new team member, David Gershony!

--- docs/app.js
+++ docs/app.js
@@ -32,4 +32,22 @@ document.addEventListener("DOMContentLoaded", function () {
             submitting.classList.add('is-hidden');
         }, 500);
     }
+
+    document.addEventListener('scroll', () => {
+        const menu = document.querySelector('.menu');
+        if (window.scrollY > 50) {
+            menu.classList.add('scrolled');
+        } else {
+            menu.classList.remove('scrolled');
+        }
+    });
+
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
 });
